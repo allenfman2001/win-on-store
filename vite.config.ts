876bd552d7a431e0fd7ade01d016f6d0ae90d29b
Allen <allenfman@berkeley.edu>
2026-01-02 00:54:00 -0800

    Add new build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/win-on-store/",
   server: {
     host: "::",
     port: 8080,
